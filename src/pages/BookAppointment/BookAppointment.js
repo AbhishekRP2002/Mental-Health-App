cb7e1385974a983795a13b9d511c4176a1592d6b
bleh1303 <liyaajijoseph@gmail.com>
2022-07-04 23:33:08 +0530

    fixed typo

--- src/pages/BookAppointment/BookAppointment.js
+++ src/pages/BookAppointment/BookAppointment.js
@@ -2,6 +2,6 @@ import React from 'react'
 
 export default function BookAppointment() {
   return (
-    <div className="book-appointment">Book Appointment secction</div>
+    <div className="book-appointment">Book Appointment section</div>
   )
 }

ed68fd70850f2b3617a8eca8c07304312405bc9b
bleh1303 <liyaajijoseph@gmail.com>
2022-07-10 16:34:58 +0530

    making sure there is no clash during pull request

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -15,7 +15,7 @@ function Navbar() {
   return (
     <>
       <IconContext.Provider value={{ color: '#fff' }}>
-        <div className='navi-bar'>
+        <div className='navbar'>
           <Link to='#' className='menu-bars'>
             <FaIcons.FaBars onClick={showSidebar} />
           </Link>
--- src/pages/Footer.js
+++ src/pages/Footer.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from 'react'
 
 function Footer() {
@@ -14,25 +13,3 @@ function Footer() {
     </div>
   )
 }
-
-=======
-import React from 'react'
-
-
-
-function Footer() {
-  return (
-    <div className="footer">
-     <span className="footer-text">
-
-    Made with <i class="fa fa-heart pulse"></i> by<a className="team-name"
-    href="https://github.com/AbhishekRP2002/Mental-Health-Appf" target="_blank">   Team HACKBIT</a>
-
-    <div></div>
-    </span>
-    </div>
-  )
-}
-
->>>>>>> parent of a03b94c (Delete src/pages directory)
-export default Footer

8796bf1a4465bfca2ada1d1ffecc5bffaf9e2b97
bleh1303 <liyaajijoseph@gmail.com>
2022-07-10 15:57:39 +0530

    Revert "Delete src/pages directory"
    This reverts commit a03b94c1355094321b3670173916c62e3d905320.

--- src/pages/Footer.js
+++ src/pages/Footer.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import React from 'react'
 
 function Footer() {
@@ -14,4 +15,24 @@ function Footer() {
   )
 }
 
+=======
+import React from 'react'
+
+
+
+function Footer() {
+  return (
+    <div className="footer">
+     <span className="footer-text">
+
+    Made with <i class="fa fa-heart pulse"></i> by<a className="team-name"
+    href="https://github.com/AbhishekRP2002/Mental-Health-Appf" target="_blank">   Team HACKBIT</a>
+
+    <div></div>
+    </span>
+    </div>
+  )
+}
+
+>>>>>>> parent of a03b94c (Delete src/pages directory)
 export default Footer

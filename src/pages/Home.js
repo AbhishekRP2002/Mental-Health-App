fb5671f82c34a2163dfba9618379ae80cea06649
GYaanii09 <87331444+GYaanii09@users.noreply.github.com>
2022-07-16 20:33:21 +0530

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import MentalApp from '../components/Home_components/MentalApp';
 import Offers from '../components/Home_components/Offers';
+import Share from '../components/Home_components/Share';
 function Home() {
   return (
     <div>
-      <MentalApp />
+    <MentalApp />
     <Offers />
+    <Share />
     </div>
   )
 }
 
 export default Home;
+
